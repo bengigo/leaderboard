67b092b9f64857848a251b5d5f23c46c2df224bf
bengigenc <genc.bengi@gmail.com>
2022-07-26 16:48:27 +0300

    Setup webpack boilerplate

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import _ from 'lodash';
+import './style.css';
+
+
+function component() {
+  const element = document.createElement("div");
+
+  // Lodash, currently included via a script, is required for this line to work
+  element.innerHTML = _.join(["Hello", "Bengi"], " ");
+  element.classList.add('hello');
+
+
+  return element;
+}
+
+document.body.appendChild(component());
