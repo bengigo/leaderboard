9fad458e56dbd643de3e1f9972be26b10043f2f0
bengigenc <genc.bengi@gmail.com>
2022-07-28 23:09:47 +0300

    Add data to API

---
+++ src/modules/postData.js
@@ -0,0 +1,17 @@
+const postData = async (name, score) => {
+  await fetch(
+    "https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/BHYQVyVnvpPxEW9OxRy4/scores",
+    {
+      method: "POST",
+      body: JSON.stringify({
+        user: name,
+        score: score,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    }
+  ).then((response) => response.json());
+};
+
+export default postData;
